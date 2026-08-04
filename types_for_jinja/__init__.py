42de1ae56c3df6b18d0069d725fd4f6c814d1f36
Kyle King <KyleKing@users.noreply.github.com>
2026-08-04 16:01:40 -0600

    bump: version 0.0.1 → 1.0.0

--- types_for_jinja/__init__.py
+++ types_for_jinja/__init__.py
@@ -2,7 +2,7 @@
 
 from ._runtime_type_check_setup import configure_runtime_type_checking_mode
 
-__version__ = '0.0.1'
+__version__ = '1.0.0'
 __pkg_name__ = 'types_for_jinja'
 
 configure_runtime_type_checking_mode()
